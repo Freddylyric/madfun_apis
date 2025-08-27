3b9f97af06e63b250c2961791bbb0d0297d4994b
mwandoski <kevimaucha19@gmail.com>
2025-08-27 16:08:50 +0300

    update IP address

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -5422,7 +5422,7 @@ class IndexController extends ControllerBase {
         $this->infologger->info(__LINE__ . ":" . __CLASS__
                 . " | dpoCallbackAction:" . json_encode($data)." IP::".$this->getClientIPAddress());
         
-        if (!in_array($this->getClientIPAddress(), ['34.250.168.72'])) {
+        if (!in_array($this->getClientIPAddress(), ['34.250.168.72','197.248.63.121'])) {
 
             return $this->dpoXMLResponse();
         }
