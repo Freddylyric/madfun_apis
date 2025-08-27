5da6fc551c4b5c69bce29ec586734713fc2bd031
mwandoski <kevimaucha19@gmail.com>
2025-08-27 16:33:46 +0300

    update IP address

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -5430,7 +5430,7 @@ class IndexController extends ControllerBase {
         $Result = $data->Result ?? null;
         
         $this->infologger->info(__LINE__ . ":" . __CLASS__
-                . " | Result:" . $Result." IP::".$this->getClientIPAddress());
+                . " dpoCallbackAction | Result:" . $Result." IP::".$this->getClientIPAddress());
         $ResultExplanation = $data->ResultExplanation ?? null;
         $TransactionToken = $data->TransactionToken ?? null;
         $TransID = $data->TransactionRef ?? null;
